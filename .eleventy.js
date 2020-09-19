032f18d4361be517827c83315795cf82879130e1
Ruben Arakelyan <ruben@arakelyan.uk>
2020-09-19 16:50:06 +0100

    Enable 404 page when serving

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,27 @@
+const fs = require("fs");
+
 module.exports = function (eleventyConfig) {
   // Copy static files
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/img");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
+
+  // 404 page config
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function(err, bs) {
+
+        bs.addMiddleware("*", (req, res) => {
+          const content_404 = fs.readFileSync("dist/404.html");
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          // Add 404 http status code in request header.
+          res.writeHead(404);
+          res.end();
+        });
+      }
+    }
+  });
 
   return {
     dir: {
